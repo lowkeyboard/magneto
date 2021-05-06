15f7e4e17c52ddf6cc15442f097ef805d61936f1
lowkeyboard <caqlarybak@gmail.com>
2021-05-06 14:47:25 +0300

    tab changing alert is working

--- pages/auth/tabpages/Home.js
+++ pages/auth/tabpages/Home.js
@@ -3,6 +3,11 @@ import React, {useEffect, useRef} from 'react';
 import {TouchableOpacity, View, Text} from 'react-native';
 
 export default function Home({navigation}) {
+  useEffect(
+    () => navigation.addListener('focus', () => alert('Screen was focused')),
+    [],
+  );
+
   return (
     <View>
       <Text>home screen</Text>
